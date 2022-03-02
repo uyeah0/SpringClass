f59c1e78e0c279ac62e0e7a7a5fb459eee8254df
uyeah0 <oki93639@gmail.com>
2022-03-02 12:07:35 +0900

    setting

---
+++ JavaBasic/src/kr/hs/study/HelloWorldEn.java
@@ -0,0 +1,12 @@
+package kr.hs.study;
+
+public class HelloWorldEn {
+
+	public static void main(String[] args) {
+		sayHello();
+	}
+	public static void sayHello() {
+		System.out.println("hello");
+	}
+
+}

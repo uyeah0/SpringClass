121037e61957fc3d68ccc50e20461c08e03f9adc
s2031@e-mirim.hs.kr <s2031@e-mirim.hs.kr>
2022-04-14 14:06:44 +0900

    [AOP] advisor bean 등록 및 beforeMethod, afterMethod 생성

--- AOP/src/main/java/kr/hs/study/main/MainClass.java
+++ AOP/src/main/java/kr/hs/study/main/MainClass.java
@@ -2,15 +2,19 @@ package kr.hs.study.main;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import kr.hs.study.beans.Designer;
-import kr.hs.study.beans.Developer;
-import kr.hs.study.beans.Publisher;
+import kr.hs.study.beans.TestBean1;
+
 
 public class MainClass {
 
 	public static void main(String[] args) {
-	
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
 		
+		TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+		obj1.method1();
+		obj1.method2();
+		
+		ctx.close();
 	}
 
 }

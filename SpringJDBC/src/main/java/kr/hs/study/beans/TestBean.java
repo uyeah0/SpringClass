2f920a51049993d63f5af8328e55880a8628f9b9
YuYeyeong <79914479+uyeah0@users.noreply.github.com>
2022-04-27 16:26:31 +0900

    [DB] 오류수정

--- SpringJDBC/src/main/java/kr/hs/study/beans/TestBean.java
+++ SpringJDBC/src/main/java/kr/hs/study/beans/TestBean.java
@@ -1,10 +1,12 @@
 package kr.hs.study.beans;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // db 테이블에 들어갈 값을 저장하거나, 
 // db 테이블에서 가져오는 값을 저장하는 곳 => dto 값이 임시로 저장되는 곳
-@Component("prototype") // 객체로 만들기
+@Component
+@Scope("prototype") // 객체로 만들기
 public class TestBean {
 	private int data1;
 	private String data2;

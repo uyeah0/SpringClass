da30a6a5fe8991c628aa72d82e4a22486ae39810
Yeyeong Yu <79914479+uyeah0@users.noreply.github.com>
2022-06-30 12:07:03 +0900

    Rename AOP/src/main/java/kr/hs/study/beans/TestBean1.java to _Spring_core/AOP/src/main/java/kr/hs/study/beans/TestBean1.java

--- _Spring_core/AOP/src/main/java/kr/hs/study/beans/TestBean1.java
+++ _Spring_core/AOP/src/main/java/kr/hs/study/beans/TestBean1.java
@@ -4,12 +4,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
 
 public class TestBean1 {
 	public void method1() {
-		System.out.println("TestBean1ÀÇ method1");
+		System.out.println("TestBean1Å”Ã‡ method1");
 		int a = 100/0;
 	}
 	
 	public void method2() {
-		System.out.println("TestBean1ÀÇ method2");
+		System.out.println("TestBean1Å”Ã‡ method2");
 	}
 
 }
